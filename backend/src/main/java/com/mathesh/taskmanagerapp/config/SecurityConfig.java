e4969f32a87ebd0078e51ace8a426fd7c0dfc97f
Mathesh <matheshmani1707@gmail.com>
2026-05-27 00:16:21 +0530

    fix: add Vercel URL to CORS allowed origins

--- backend/src/main/java/com/mathesh/taskmanagerapp/config/SecurityConfig.java
+++ backend/src/main/java/com/mathesh/taskmanagerapp/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://seyal-flow.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
